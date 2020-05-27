f0d07fd5492a822cf3eca53c894b978660811fd6
ManoleStefan <63060910+ManoleStefan@users.noreply.github.com>
2020-05-27 17:39:19 +0300

    Tema12

---
+++ pb1.kt
@@ -0,0 +1,35 @@
+import kotlin.collections.zipWithNext
+
+
+fun main() {
+
+    var lista1   = listOf(1,4,15,27,23,78,32,89,34,61).toMutableList()
+    var lista2: List<Pair<Int, Int>>
+    var i  = 0
+    var sum     = 0
+
+
+    lista1 = lista1.filter { it>5 }.toMutableList()
+
+    lista2 = lista1.zipWithNext()
+
+    lista1 = listOf<Int>().toMutableList()
+
+    lista2.onEach {
+        lista1 = (lista1 + it.first*it.second).toMutableList()
+    }
+
+    println(lista1)
+
+    while(i <= lista1.lastIndex)
+    {
+        if(i % 2 == 0)
+        {
+            sum += lista1.get(i)
+        }
+        i++
+    }
+
+    println(sum)
+
+}
